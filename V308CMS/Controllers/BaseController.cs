1ea0934583af2d9591f08b154d9728cceb1ce3a9
Neal <toaihv@gmail.com>
2017-05-17 17:15:16 +0700

    add BaseController.cs

---
+++ V308CMS/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using V308CMS.Data;
+
+namespace V308CMS.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        private static readonly V308CMSEntities MEntities = new V308CMSEntities() ;
+
+        protected BaseController()
+        {           
+            NewsService = new NewsRepository(MEntities);
+        }
+
+        protected V308CMSEntities MpStartEntities => MEntities;
+        protected NewsRepository NewsService { get; }
+
+        protected string FindView(string name)
+        {
+            return Theme.viewPage(name);
+        }
+    }
+}
