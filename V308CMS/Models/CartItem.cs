1b781e33c5ecb5dcb5d281025302591705174fc4
Neal <toaihv@gmail.com>
2017-05-18 17:31:33 +0700

    add CartItem.cs in Models folder

---
+++ V308CMS/Models/CartItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using V308CMS.Data;
+
+namespace V308CMS.Models
+{
+    public class CartItem : IEquatable<CartItem>
+    {
+        public CartItem(Product productItem)
+        {
+            ProductItem = productItem;           
+        }
+        //San pham
+        public Product ProductItem { get; set; }    
+        //So luong
+        public int Quantity { get; set; }
+        //Don gia
+        public double UnitPrice
+        {
+            get {
+                if (ProductItem.Price != null)
+                    return ProductItem.Price.Value;
+                return 0;
+            }
+        }
+        public int Voucher { get; set; }
+        public int SaleOff => ProductItem.SaleOff ?? 0;
+
+
+        public double TotalPrice => SaleOff > 0 ? 
+            Quantity * ((UnitPrice - ((UnitPrice / 100) * SaleOff))) : 
+            Quantity * UnitPrice;
+
+        public bool Equals(CartItem other)
+        {
+            return other != null && (other.ProductItem.ID == this.ProductItem.ID);
+        }
+    }
+}
