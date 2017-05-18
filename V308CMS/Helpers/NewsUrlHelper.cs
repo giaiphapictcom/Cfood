586ec3af58c1aed641f27823311c7204b42ec3c3
NEAL HOANG <NEAL HOANG@NEALHOANG-PC>
2017-05-19 00:36:49 +0700

    no message

--- V308CMS/Helpers/NewsUrlHelper.cs
+++ V308CMS/Helpers/NewsUrlHelper.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using V308CMS.Data;
 
 namespace V308CMS.Helpers
 {
-    public static partial class MpStartUrlHelper
+    public static  class MpStartUrlHelper
     {
         public static string NewsIndexUrl(this UrlHelper helper, int page = 1, string controller = "news", string action = "index")
         {
