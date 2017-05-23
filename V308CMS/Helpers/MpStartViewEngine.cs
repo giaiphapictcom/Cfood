761988ec884aa239b98f80c43ab683f9411f8165
DESKTOP-HOT4L8K\QuanICT <hongquan2712@gmail.com>
2017-05-23 07:34:42 +0700

    fix view for admin contrller

--- V308CMS/Helpers/MpStartViewEngine.cs
+++ V308CMS/Helpers/MpStartViewEngine.cs
@@ -9,23 +9,47 @@ namespace V308CMS.Helpers
 {
     public class MpStartViewEngine: RazorViewEngine
     {
-        public MpStartViewEngine()
+        public MpStartViewEngine(Boolean setDefault = false)
         {
-            var viewPath = string.Format("~/Views/themes/{0}", ConfigHelper.Domain);
+            var viewPath = "";
+            if (setDefault)
+            {
+                var routeValues = HttpContext.Current.Request.RequestContext.RouteData.Values;
+                string ControllerName = routeValues["controller"].ToString();
+                ControllerName = ControllerName.First().ToString().ToUpper() + String.Join("", ControllerName.Skip(1));
+                viewPath = string.Format("~/Views/{0}",ControllerName);
+            }
+            else {
+                viewPath = string.Format("~/Views/themes/{0}", ConfigHelper.Domain);
+            }
+            
             var viewRealPath =  HttpContext.Current.Server.MapPath(viewPath);
             var listDir = Directory.GetDirectories(viewRealPath, "*",SearchOption.AllDirectories);
-            if (listDir.Length == 0){
-               throw  new Exception("Can't find views directory to setup.");
-            }
-            var locations = new string[listDir.Length];
-            for (int i = 0; i < listDir.Length; i++)
+            if (listDir.Length == 0)
             {
-                var viewDir = listDir[i].Replace(viewRealPath, "").Replace(@"\", "/");
-                locations[i] = viewPath + viewDir + "/{0}.cshtml";
+                //throw  new Exception("Can't find views directory to setup.");
+                var locations = new string[1];
+                //var viewDir = listDir[i].Replace(viewRealPath, "").Replace(@"\", "/");
+                //locations[i] = viewPath + viewDir + "/{0}.cshtml";
+
+                locations[0] = viewPath+"/{0}.cshtml";;
+                this.ViewLocationFormats = locations;
+                this.PartialViewLocationFormats = locations;
+                this.MasterLocationFormats = locations;
+                
             }
-            this.ViewLocationFormats = locations;
-            this.PartialViewLocationFormats = locations;
-            this.MasterLocationFormats = locations;
+            else {
+                var locations = new string[listDir.Length];
+                for (int i = 0; i < listDir.Length; i++)
+                {
+                    var viewDir = listDir[i].Replace(viewRealPath, "").Replace(@"\", "/");
+                    locations[i] = viewPath + viewDir + "/{0}.cshtml";
+                }
+                this.ViewLocationFormats = locations;
+                this.PartialViewLocationFormats = locations;
+                this.MasterLocationFormats = locations;
+            }
+            
 
 
         }
