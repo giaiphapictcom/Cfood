d4db72cefeadfa3041441f578138f62275a486a4
NEAL HOANG <NEAL HOANG@NEALHOANG-PC>
2017-05-24 16:24:08 +0700

    Commit ConfigHelper.cs

---
+++ V308CMS.Admin/Helpers/ConfigHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using V308CMS.Common;
+
+namespace V308CMS.Admin.Helpers
+{
+    public class ConfigHelper
+    {
+        public static string ImageDomain
+        {
+            get { return Configs.GetItemConfig("ImageDomain"); }
+        }
+    }
+}
