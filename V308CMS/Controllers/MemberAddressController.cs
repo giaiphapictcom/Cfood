ff0ea33ef50d1227b022437a704ab6cce47dde94
Neal <toaihv@gmail.com>
2017-06-07 15:27:59 +0700

    Commit by Toaihv

---
+++ V308CMS/Controllers/MemberAddressController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace V308CMS.Controllers
+{
+    public class AccountAddressController : Controller
+    {
+        //
+        // GET: /Address/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
