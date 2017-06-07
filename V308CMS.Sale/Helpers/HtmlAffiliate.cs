94bb7de725fed2fc2a5301e1bdd75646fa9a8f6c
DESKTOP-HOT4L8K\QuanICT <hongquan2712@gmail.com>
2017-06-07 15:00:22 +0700

    no message

---
+++ V308CMS.Sale/Helpers/HtmlAffiliate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace V308CMS.Sale.Helpers
+{
+    public class HtmlAffiliate
+    {
+        public static string MenuActive(string uri="",string classSelected = "selected"){
+            string classname = "";
+            string baseUrl = HttpContext.Current.Request.FilePath;
+            if (baseUrl[0] == '/') {
+                baseUrl = baseUrl.TrimStart('/');
+            }
+            if (uri == baseUrl.Split('/').FirstOrDefault()) {
+                classname = classSelected;
+            }
+            else if (uri==baseUrl)
+            {
+                classname = classSelected;
+            }
+            return classname;
+        }
+    }
+}
