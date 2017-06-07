94c5d5fb9434f1e499e7c490faff624f0efdc40c
Neal <toaihv@gmail.com>
2017-06-07 15:27:01 +0700

    Update MemberWishListController

---
+++ V308CMS/Controllers/MemberWishListController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using V308CMS.Helpers;
+
+namespace V308CMS.Controllers
+{
+   
+    public class MemberWishListController : BaseController
+    {
+        //
+        // GET: /WishList/
+        [Authorize]
+        public ActionResult Index()
+        {
+            var wishlist = ProductWishlistService.GetListWishlist(AuthenticationHelper.CurrentUser);
+            var listProductWishlist = ProductsService.GetListProductWishlist(wishlist);
+            return View("Wishlist.Index", listProductWishlist);
+        }
+
+        [HttpPost]
+        public JsonResult AddToWishList(int id)
+        {
+            if (!AuthenticationHelper.IsAuthenticate)
+            {
+                return Json(new {code = 0, message = "Bạn cần đăng nhập để thực hiện chức năng này."});
+            }
+            var result = ProductWishlistService.AddItemToWishlist(id, AuthenticationHelper.CurrentUser);
+            if (result == "exist")
+            {
+                return Json(new { code = 0, message = "Sản phẩm đã có trong danh sách yêu thích." });
+            }
+            return Json(new { code = 1, message = "Thêm sản phẩm vào danh sách yêu thích thành công." });
+        }
+
+        public JsonResult RemoveItemFromWishList(int id)
+        {
+            if (!AuthenticationHelper.IsAuthenticate)
+            {
+                return Json(new {code = "require_login", message = "Bạn cần đăng nhập để thực hiện chức năng này."});
+            }
+            var result = ProductWishlistService.RemoveItemFromWishlist(id, AuthenticationHelper.CurrentUser);
+            if (result == "userid_invalid")
+            {
+                return Json(new { code = 0, message = "Tên tài khoản không chính xác." });
+            }
+            if (result == "not_exist")
+            {
+                return Json(new { code = 0, message = "Danh sách sản phẩm yêu thích trống." });
+            }
+            if (result == "invalid")
+            {
+                return Json(new { code = 0, message = "Sản phẩm không thuộc trong danh sách yêu thích." });
+            }
+            return Json(new { code = result, message = "Xóa sản phẩm khỏi danh sách yêu thích thành công." });
+        }
+
+
+    }
+}
