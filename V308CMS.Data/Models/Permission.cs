764cff6cc65ddc8eb97e0175fd5d1908cc170d41
Neal <toaihv@gmail.com>
2017-06-07 14:15:54 +0700

    Commit by toaihv

---
+++ V308CMS.Data/Models/Permission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using V308CMS.Data.Metadata;
+
+namespace V308CMS.Data.Models
+{
+    [Table("permission")]
+    [MetadataType(typeof(PermissionMetadata))]
+    public  class Permission
+    {
+        public Permission()
+        {
+            this.Roles = new HashSet<Role>();
+        }
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public string Action { get; set; }
+        public int Value { get; set; }
+        public byte Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<Role> Roles { get; set; }
+
+    }
+}
---
+++ V308CMS.Data/Models/RolePermission.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace V308CMS.Data.Models
+{
+    public class RolePermission
+    {
+        [Key]
+        [Required]
+        public int RoleId { get; set; }
+        [Key]
+        [Required]
+        public int PermissionId { get; set; }
+    }
+}
