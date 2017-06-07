67b84fa26beed0a964258a4b7cba361d1e309dd4
Neal <toaihv@gmail.com>
2017-06-07 14:17:48 +0700

    Commit by toaihv

---
+++ V308CMS.Data/Metadata/PermissionMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace V308CMS.Data.Metadata
+{
+    public class PermissionMetadata
+    {        
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int GroupId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Action { get; set; }
+        [Required]
+        public int Value { get; set; }
+    }
+}
---
+++ V308CMS.Data/Metadata/RolePermissionMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace V308CMS.Data.Metadata
+{
+    public class RolePermissionMetadata
+    {
+        public int RoleId { get; set; }
+        public int PermissionId { get; set; }
+    }
+}
