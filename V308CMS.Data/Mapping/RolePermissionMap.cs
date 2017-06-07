ab1e799a9bff5fea6a57269a10b7e0550a2ab10b
Neal <toaihv@gmail.com>
2017-06-07 14:19:23 +0700

    Commit mapping toaihv

---
+++ V308CMS.Data/Mapping/RolePermissionMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using V308CMS.Data.Models;
+
+namespace V308CMS.Data.Mapping
+{
+    public class RolePermissionMap: EntityTypeConfiguration<RolePermission>
+    {
+        public RolePermissionMap()
+        {
+            HasKey(t => t.RoleId);
+            HasKey(t => t.PermissionId);
+           
+            ToTable("role_permission");
+            
+            Property(t => t.RoleId).HasColumnName("role_id");
+            Property(t => t.PermissionId).HasColumnName("permission_id");
+        }
+    }
+}
