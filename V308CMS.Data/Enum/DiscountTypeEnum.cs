d79b395b89ffb0e8ccbdb6fbda0a2f4dd3081734
NEALHOANG-PC\NEAL HOANG <toaihv@gmail.com>
2017-06-18 10:23:02 +0700

    Add enums

---
+++ V308CMS.Data/Enum/DiscountTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace V308CMS.Data.Enum
+{
+    public enum DiscountTypeEnum
+    {
+        [Description("Giảm theo đơn hàng")]
+        MpStart = 1,
+        [Description("Giảm theo sản phẩm")]
+        Affiliate = 2
+    }
+}
