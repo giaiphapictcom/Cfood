82a96a8f7fcb586fe8cd33d71e349d53417f9be1
NEALHOANG-PC\NEAL HOANG <toaihv@gmail.com>
2017-07-01 00:49:23 +0700

    Commit by toaihv

---
+++ V308CMS/Helpers/MemberWishListUrlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace V308CMS.Helpers
+{
+    public static class MemberWishListUrlHelper
+    {
+        public static string MemberWishListIndexUrl(this UrlHelper helper, string controller = "memberwishlist",
+        string action = "index")
+        {
+            return helper.Action(action, controller);
+        }
+
+    }
+}
